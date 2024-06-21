672200ec80e7d9c9a535afc0cbabca450c8d417d
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-06-21 17:40:16 -0300

    chore: Update main.ts to use array literal syntax for providers

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,5 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient()
-  ],
+  providers: [provideHttpClient()],
 });
